cd1caf6acd4164a86bee795e3b48e70a1521bb20
araizaes <araizaes@mail.fresnostate.edu>
2017-05-13 00:45:10 -0700

    http completed

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule, XHRBackend } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
@@ -10,11 +11,13 @@ import { CategoryListPipe } from './category-list.pipe'
 import { MediaItemFormComponent } from './media-item-form.component';
 import { MediaItemSercice } from './media-item.service';
 import { lookupListToken, lookupLists } from './providers';
+import { MockXHRBackend } from './mock-xhr-backend';
 
 @NgModule({
   imports: [
     BrowserModule
     ReactiveFormsModule
+    HttpModule
   ],
   declarations: [
     AppComponent,
@@ -26,7 +29,8 @@ import { lookupListToken, lookupLists } from './providers';
   ],
   providers: [
     MediaItemService
-    { provide: lookupListToken, useValue: lookuplists}
+    { provide: lookupListToken, useValue: lookuplists },
+    { provide: XHRBackend, useClass: MockXHRBackend }
   ],
   bootstrap: [
     AppComponent
--- app/media-item-list.component.ts
+++ app/media-item-list.component.ts
@@ -8,16 +8,27 @@ import { MediaItemService } from './media-item.service';
   styleUrls: ['app/media-item-list.component.css']
 })
 export class MediaItemListComponent {
-    mediaItems;
+    medium = '';
+    mediaItems = [];
 
   constructor(private mediaItemSercice: MediaItemService) {}
   
   ngOnInit() {
-    this.mediaItems = this.MediaItemSercice.get();
-  }
-  
-  onMediaItemDelete(mediaItem) {
+    this.getMediaItems(this.medium);
+    }
+    
+    onMediaItemDelete(mediaItem) {
   this.mediaItemSercice.delete(mediaItem);
+    .subscribe(() => {
+    this.getMediaItems(this.medium);
+    });
 }
 
+    getMediaItems(medium) {
+    this.medium = medium;
+    this.mediaItemService.get(medium)
+    .subscribe(mediaItems => {
+    this.mediaItems = mediaItems
+    });
+    }
 }
